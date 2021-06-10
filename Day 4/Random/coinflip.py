104a8860633e8ad07587cc57ddd4bdc69a7a327d
Nhat Vo <nhat.vo@robinsonpharma.com>
2021-06-10 12:50:56 -0700

    -m

---
+++ Day 4/Random/coinflip.py
@@ -0,0 +1,7 @@
+import random
+
+side = random.randint(1, 2)
+if side ==1:
+    print('head')
+else:
+    print('tail')
---
+++ Day 4/Random/main.py
@@ -0,0 +1,5 @@
+import random
+import my_module
+
+random_float = random.random()
+print(random_float*5)
