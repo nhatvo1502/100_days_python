18a928af2a2acd36f3a91fb55e317861cf6263fa
Nhat Vo <nhatvo31415@gmail.com>
2025-10-12 21:16:38 -0700

    15

---
+++ task.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name")
+name_length = len(user_name)
+
+print(type("Number of letters in your name: "))
+print(type(name_length))
+
+print("Number of letter in your name: " + str(name_length))
