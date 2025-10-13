847adce67d0943bdd03e1fc6383996fe11188530
Nhat Vo <nhatvo31415@gmail.com>
2025-10-12 21:18:32 -0700

    16

--- task.py
+++ task.py
@@ -1,7 +1,3 @@
-user_name = input("Enter your name")
-name_length = len(user_name)
+var = 3 * 3 + 3 / 3 - 3
 
-print(type("Number of letters in your name: "))
-print(type(name_length))
-
-print("Number of letter in your name: " + str(name_length))
+print(var)
