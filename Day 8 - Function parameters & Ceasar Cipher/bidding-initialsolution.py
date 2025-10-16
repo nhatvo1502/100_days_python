f24af1faf443afb042c01fa2c4cdb209995f2e66
Nhat Vo <nhatvo31415@gmail.com>
2025-10-15 18:12:28 -0700

    70

--- Day 8 - Function parameters & Ceasar Cipher/bidding-initialsolution.py
+++ Day 8 - Function parameters & Ceasar Cipher/bidding-initialsolution.py
@@ -1,3 +1,32 @@
+import os
+# import sys
+
+# # Support Functions
+# def clear_console():
+#     """Clears the console screen based on the operating system."""
+#     if sys.platform.startswith('win'):
+#         # For Windows
+#         os.system('cls')
+#     else:
+#         # For Linux, macOS, and other Unix-like systems
+#         os.system('clear')
+
+# Program
+
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
 print("Welcome to bidder")
 
 dict = {}
@@ -6,9 +35,23 @@ while True:
     user_bid = float(input("What is your bid?: $ "))
     dict.update({user_name: user_bid})
     newBidder = input("Are there any other bidders? Type 'yes or no'.\n")
+    os.system('cls')
     if newBidder == 'no':
         break
 
-highest_bidder = max(dict, key=dict.get)
-highest_bid = dict[highest_bidder]
-print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+def find_highest_bidder_max(dict):
+    highest_bidder = max(dict, key=dict.get)
+    highest_bid = dict[highest_bidder]
+    return highest_bidder, highest_bid
+
+def find_highest_bidder_loop(dict):
+    highest_bidder = ''
+    highest_bid = 0
+    for bidder in dict:
+        if highest_bid < dict[bidder]:
+            highest_bid = dict[bidder]
+            highest_bidder = bidder
+    return highest_bidder, highest_bid
+
+bidder, bid = find_highest_bidder_loop(dict)
+print(f"The winner is {bidder} with a bid of ${bid}")
