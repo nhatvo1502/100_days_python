0a73d12187d546657fa56584b18b75a242517bbe
nhatvo1502 <nhatvo31415@gmail.com>
2025-10-24 09:57:16 -0700

    turtle draw math and shapes

--- Day 18 - Turtle/myturtle.py
+++ Day 18 - Turtle/myturtle.py
@@ -5,9 +5,19 @@ jim = Turtle()
 jim.shape("turtle")
 jim.color("red")
 
-# for i in range(4):
-#     jim.forward(100)
-#     jim.right(90)
+side_length = 100
+side_number = 3
+# while True:
+#     for i in range(side_number):
+#         jim.forward(side_length)
+#         jim.right(180/side_number)
+#     side_number+=1
+for a in range(10):
+    for i in range(side_number):
+            angle = 180-(((side_number-2)*180)/side_number)
+            jim.forward(side_length)
+            jim.right(angle)
+    side_number+=1
 
 screen = Screen()
 screen.exitonclick()
