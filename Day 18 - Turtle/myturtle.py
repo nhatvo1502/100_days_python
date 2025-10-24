b7aa01c12d0ea10e77ef09ab75ba9e92b18e53c5
nhatvo1502 <nhatvo31415@gmail.com>
2025-10-24 09:33:56 -0700

    turtle

---
+++ Day 18 - Turtle/myturtle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+jim = Turtle()
+
+jim.shape("turtle")
+jim.color("red")
+
+# for i in range(4):
+#     jim.forward(100)
+#     jim.right(90)
+
+screen = Screen()
+screen.exitonclick()
