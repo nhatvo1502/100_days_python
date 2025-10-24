62920baa8ddee406b7ed56f00024c4fa2b0cb416
nhatvo1502 <nhatvo31415@gmail.com>
2025-10-24 10:05:52 -0700

    turtle draw flowers

--- Day 18 - Turtle/myturtle.py
+++ Day 18 - Turtle/myturtle.py
@@ -1,6 +1,30 @@
 from turtle import Turtle, Screen
+import random
 
 jim = Turtle()
+
+colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "brown",
+    "black",
+    "gray",
+    "cyan",
+    "magenta",
+    "lime",
+    "gold",
+    "turquoise",
+    "violet",
+    "indigo",
+    "navy",
+    "coral",
+    "salmon"
+]
 
 jim.shape("turtle")
 jim.color("red")
@@ -12,11 +36,22 @@ side_number = 3
 #         jim.forward(side_length)
 #         jim.right(180/side_number)
 #     side_number+=1
-for a in range(10):
-    for i in range(side_number):
-            angle = 180-(((side_number-2)*180)/side_number)
-            jim.forward(side_length)
-            jim.right(angle)
+
+def random_color():
+    jim.color(random.choice(colors))
+
+def draw_shape(sides):
+    for i in range(sides):
+        if i == 0:
+            angle = (((sides-2)*180)/sides)
+        else:
+            angle = 180-(((sides-2)*180)/sides)
+        jim.forward(side_length)
+        jim.right(angle)
+
+for i in range(20):
+    random_color()
+    draw_shape(side_number)
     side_number+=1
 
 screen = Screen()
