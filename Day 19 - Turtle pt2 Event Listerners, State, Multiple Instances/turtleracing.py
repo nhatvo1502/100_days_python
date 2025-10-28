b6807dc38ecbcbe30d30e11cffbb7e78373470d4
nhatvo1502 <nhatvo31415@gmail.com>
2025-10-28 16:37:26 -0700

    turtle racing

--- Day 19 - Turtle pt2 Event Listerners, State, Multiple Instances/turtleracing.py
+++ Day 19 - Turtle pt2 Event Listerners, State, Multiple Instances/turtleracing.py
@@ -2,24 +2,31 @@ from turtle import Turtle, Screen
 from tkinter import messagebox, Tk
 import random
 
-colors = ['red', 'blue', 'green']
+COLORS = [
+    "red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "black", "gray", "cyan", "magenta", "gold", "silver", "coral", "turquoise", "lime", "navy", "maroon",
+    "olive", "teal", "aqua", "indigo", "violet", "khaki", "salmon", "plum", "orchid",
+    "tan", "tomato", "chocolate", "crimson", "lavender", "azure", "peru", "seagreen", "slateblue", "skyblue",
+    "darkgreen", "darkred", "darkblue", "darkorange", "darkviolet", "darkgray", "deepskyblue", "lightgreen", "lightblue", "lightcoral",
+    "lightpink", "lightsalmon", "lightgray", "lightcyan", "mediumblue", "mediumseagreen", "mediumvioletred", "royalblue", "sienna"
+]
+FINISH_LINE = 400 
 
-def create_turtles():
-    khoa = Turtle()
-    terry = Turtle()
-    nhat = Turtle()
-    turtles = [khoa, terry, nhat]
+def create_turtles(num):
+    random.shuffle(COLORS)
+    turtles = []
+    color_index = []
+    for turtle_index in range(0, num):
+        jennie = Turtle()
+        jennie.shape('turtle')
+        jennie.color(COLORS[turtle_index])
+        color_index.append(COLORS[turtle_index])
+        jennie.shapesize(2, 2)
+        turtles.append(jennie)
     
-    for i in range(0, 3):
-        turtle = turtles[i]
-        turtle.shape('turtle')
-        turtle.color(colors[i])
-        turtle.shapesize(2, 2)
-    
-    return turtles
+    return turtles, color_index
 
 # Transit turtles to starting
-def set_starting_point(turtles):
+def set_starting_point(turtles, color_index):
     distant = 600/(len(turtles) -1)
     for _ in range(0, len(turtles)):
         pos = -300 + _*distant
@@ -28,20 +35,38 @@ def set_starting_point(turtles):
         print(len(turtles))
         print(_)
         print(turtles[_].pos())
+        turtles[_].write(f"                 {turtles[_].color()[1]}")
+    
+    return turtles, color_index
+
+def prompt_turtle_number(screen):
+    tur_num = 0
+    while tur_num < 2 or tur_num > 10:
+        tur_num = screen.numinput('turtle number', "Enter the number of turles between 1-10: ")
+    return int(tur_num)
+
+def make_bet(screen, color_index):
+    isCorrected = False
+    color_selection = ', '.join(color_index)
+    user_bet = screen.textinput('title', f'Which turtle do you bet on? ({color_selection})')
+    while not isCorrected:  
+        if user_bet.lower() in COLORS:
+            isCorrected = True
+        else:
+            user_bet = screen.textinput('tittle', f'Incorrected input! Which turtle do you bet on? ({color_selection})')
 
 # Race
 def race(turtles):
-    current_winner = Turtle()
-    current_winner.hideturtle()
-    current_winner.penup()
-    current_winner.goto(-500, 400)
+    for turtle in turtles:
+        turtle.clear()
     isWin = False
     while not isWin:
         for turtle in turtles:
             turtle.forward(random.randint(0, 5))
             print(turtle.pos()[0])
-            if turtle.pos()[0] >= 300:
+            if turtle.pos()[0] >= FINISH_LINE:
                 isWin = True
+                turtle.write(f"                 {turtle.color()[1]}")
                 return turtle
 
 def annouce_winner(winner, user_predict):
@@ -53,25 +78,16 @@ def annouce_winner(winner, user_predict):
         messagebox.showinfo('Race result', f"{winner.color()[1]} crossed the finish line, you lose!")
 
 def main():
-    random.shuffle(colors)
     
-    turtles = create_turtles()
-
     screen = Screen()
-    set_starting_point(turtles)
-    isCorrected = False
-    user_bet = screen.textinput('title', 'Which turtle do you bet on? (blue, red, green)')
-    while not isCorrected:  
-        if user_bet == 'blue' or user_bet == 'red' or user_bet == 'green':
-            isCorrected = True
-        else:
-            user_bet = screen.textinput('tittle', 'Incorrected input! Which turtle do you bet on? (blue, red, green)')
+
+    turtles_number = prompt_turtle_number(screen)
+
+    turtles, color_index = set_starting_point(*create_turtles(turtles_number))
+
+    user_bet = make_bet(screen, color_index)
+
     winner = race(turtles)
-    print(f'The winner is {winner.color()[1]}')
-    winner.write(f'I win! fuck you guys')
-    turtles.remove(winner)
-    for _ in turtles:
-        _.write(f'aww shit')
 
     annouce_winner(winner, user_bet)
 
